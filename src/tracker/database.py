760c88ba1def6dd3a08b819c3d5441ceab3c4705
Harry Taylor <taylor23harry@gmail.com>
2022-08-10 00:07:41 +0200

    Added remove_security()  + Refactoring

--- src/tracker/database.py
+++ src/tracker/database.py
@@ -17,9 +17,8 @@ class Database:
 
         db_uri = environ.get("DATABASE_CONNECTION_URI")
         #self.engine = create_engine(db_uri, echo=True)
-
         self.conn = psycopg2.connect(f'dbname={environ.get("DB_NAME")} user={environ.get("DB_USER")}')
-
+        self.cur = self.conn.cursor()
 
     def add_security(self, ticker: str, type: str, amount: int or float):
         """Add security to database.
@@ -42,29 +41,69 @@ class Database:
         
         # --- Check whether the security is already in the database --- #
 
-        cur = self.conn.cursor()
-        cur.execute(f"SELECT amount FROM holdings WHERE ticker = '{ticker}';")
-        try:
-            already_owned = cur.fetchone()[0]
-        except TypeError:
-            # already_owned is None, therefore not subscriptable.
-            already_owned = None
+        already_owned = self.get_amount(ticker)
 
         # ------------------- Database Manipulation ------------------- #
 
         if already_owned is None:
             sql = f"INSERT INTO holdings (ticker, type, amount) VALUES ('{ticker}', '{type}', {amount});"
-            cur = self.conn.cursor()
-            cur.execute(sql)
+            self.cur.execute(sql)
         else:
             new_amount = already_owned + amount
 
             sql = f"UPDATE holdings SET amount = {new_amount} WHERE ticker = '{ticker}';"
-            cur = self.conn.cursor()
-            cur.execute(sql)
+            self.cur.execute(sql)
         self.conn.commit()
 
+    def remove_security(self, ticker: str, amount: int or float):
+        """Removes securities from database.
+            Essentially a sell order.
+        params:
+            ticker: consists of two parts: {SYMBOL_NAME}.{EXCHANGE_ID},
+                then you can use, for example, MCD.MX for Mexican Stock Exchange.
+                or MCD.US for NYSE.
+            amount: Quantity to remove."""
+        
+        # Read database and see if ticker exists
+        already_owned = self.get_amount(ticker)
 
+        # If the security is already owned, decrease 'amount' in DB by the 'amount' parameter.
+        # Delete the row if it has been sold entirely.
+
+        if already_owned is not None:
+            new_amount = already_owned - amount
+
+            if new_amount > 0:
+                sql = f"UPDATE holdings SET amount = '{new_amount}' WHERE ticker = '{ticker}';"
+                self.cur.execute(sql)
+
+            elif new_amount == 0:
+                sql = f"DELETE FROM holdings WHERE ticker = '{ticker}'"
+                self.cur.execute(sql)
+
+            else:
+                raise Exception("Cannot sell more than holdings.")
+
+            # Commit changes to DB.
+            self.conn.commit()
+        else:
+            raise Exception("Cannot sell non-existing security.")
+
+        # !TODO if 'amount' in DB reaches 0, remove it.
+
+    def get_amount(self, ticker: str) -> int or float or None:
+        """Returns the quantity of a given security in DB.
+        params:
+            ticker: consists of two parts: {SYMBOL_NAME}.{EXCHANGE_ID},
+                then you can use, for example, MCD.MX for Mexican Stock Exchange.
+                or MCD.US for NYSE."""
+
+        self.cur.execute(f"SELECT amount FROM holdings WHERE ticker = '{ticker}';")
+        try:
+            return self.cur.fetchone()[0]
+        except TypeError:
+            # already_owned is None, therefore not subscriptable.
+            return None
 
 if __name__ == '__main__':
     db = Database()
